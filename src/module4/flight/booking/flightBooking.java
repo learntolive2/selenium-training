d127ec826dbf42bd81deec70cb55c8571fcc4dac
learntolive2 <vinayshah@hotmail.com>
2019-08-18 19:16:48 -0700

    After aug. 18, 2019 - unable to perform click on Done button in date-picker dialog and selecting number of passenger pop-out.  Possible there is new Help pop-out element which cannot be dismiss.  Applied work-around to dissmiss date-picker and # of passenger dialog.

--- src/module4/flight/booking/flightBooking.java
+++ src/module4/flight/booking/flightBooking.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /*Author:  Vinay Shah
@@ -19,8 +21,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  Automate the same using (https://www.goindigo.in/?linkNav=home_header) portal.
  The steps are as below.
  Steps:
- 1. Launch the browser with “https://www.goindigo.in/?linkNav=home_header”
- 2. Select “One way” in the option
+ 1. Launch the browser with https://www.goindigo.in/?linkNav=home_header
+ 2. Select One way in the option
  3. Fill all the details specified above
  4. Click on search button
  5. Wait for the page to load and list all the flights available
@@ -42,7 +44,7 @@ public class flightBooking {
 		
 		driver.get("https://www.goindigo.in/?linkNav=home_header");
 
-		// Step 2. Select “One way” in the option
+		// Step 2. 
 		driver.findElement(By.xpath("//label[@for = 'oneWayTrip']")).click();
 		
 		//Step 3. Fill all the details specified above
@@ -50,26 +52,87 @@ public class flightBooking {
 		WebElement departSearch = driver.findElement(By.xpath("//*[@id='bookFlightTab']/form/div[3]/div[1]/div[1]/div"));
 		myWait.until(ExpectedConditions.visibilityOf(departSearch));
 		
-		
-		//driver.findElement(By.xpath("//*[@id='bookFlightTab']/form/div[3]/div[1]/div[1]/div")).click();
-		
-		Thread.sleep(3000);
 		//Type departing city
-		//myWait.until(arg0)
-		driver.findElement(By.xpath("//*[@id='orSrcModal']//input")).sendKeys("Bengaluru");
+		departSearch.click();
 		
-		//Select city
-		driver.findElement(By.xpath("//*[@id='orSrcModal']//div[@data-iata='BLR' and contains(@class, 'pop-dest-stn')]"));
+		////div[@id='bookFlightTab']//input[@data-parsley-airport-message='Please select a Source'][1]
+		//driver.findElement(By.xpath("//div[@id='bookFlightTab']//input[@data-parsley-airport-message='Please select a Source'][1]")).clear();
+		//driver.findElement(By.xpath("//*[@id='orSrcModal']//div[@data-iata='BLR' and contains(@class, 'pop-dest-stn')]")).click();	//Selecting from pop-up auto complete result
+		driver.findElement(By.xpath("//div[@id='bookFlightTab']//input[@data-parsley-airport-message='Please select a Source'][1]")).sendKeys("BLR", Keys.ENTER);
 		
-		//Search for destination city - Lucknow
-		driver.findElement(By.xpath("//*[@id='orDestModal']//input")).sendKeys("Lucknow");
+		//After entering the departing city and hit enter, focus automatically goes into destination selection
+		driver.findElement(By.xpath("//div[@id='bookFlightTab']//input[@data-parsley-airport-message='Please select a Destination'][1]")).sendKeys("LKO", Keys.ENTER);
+	
+		Thread.sleep(3000);
 		
-		//Select destination airport from auto-complete
-		//*[@id='orDestModal']//div[@data-iata='LKO']
-		driver.findElement(By.xpath("//*[@id='orDestModal']//div[@data-iata='LKO']"));
+		//After destination city, date field gets auto focus and opens the dropdown for date pick
+		//Since have to select the current date, its safe to click accept default selection and click Done to close the dialog
+		//click Done to close date picker dialog
+
+		String strDateCloseBtn = "//div[@id='bookFlightTab']//a[contains(@class, 'dateClose') and text() = 'Done']"; 
 		
-		Thread.sleep(5000);
-		driver.quit();
+		//Not working
+		//Actions act = new Actions(driver);
+		//act.moveToElement(driver.findElement(By.xpath(strDateCloseBtn)));
+		
+		//Try2 - click in date-picker title (in its modal window) -- Didn't help
+		//myWait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//div[@class='ui-datepicker-title'][1]")))).click();
+		
+		//Try3 - Instead of clicking Done, let's try selecting an active date.  Which is always current - Today's date selected
+		driver.findElement(By.xpath("//a[contains(@class, 'ui-state-active')]")).click();
+		
+		//Aug 18, 2019 - some reason this stopped working.  There was an additional pop-up Help element, which cannot be dismiss
+		//	Exception in thread "main" org.openqa.selenium.WebDriverException: unknown error: Element is not clickable at point (647, 688)
+		//  	(Session info: chrome=75.0.3770.80)
+		  
+		//driver.findElement(By.xpath("//div[@id='bookFlightTab']//a[contains(@class, 'dateClose') and text() = 'Done']")).click();
+		//myWait.until(ExpectedConditions.elementToBeClickable
+		//		(driver.findElement(By.xpath("//div[@id='bookFlightTab']//a[contains(@class, 'dateClose') and text() = 'Done']")))).click();
+		
+		//Click passenger to open dropdown to select count
+		//xpath = //div[@id='bookFlightTab']//input[@name='passenger']
+		driver.findElement(By.name("passenger")).click();
+		
+		//
+		//To increase # of adult passenger count
+		////div[@id='bookFlightTab']//li[@class='adult-pax-list']//button[contains(@class, 'pax-plus')]
+		//Default adult count = 1 (cannot lower it).
+		driver.findElement(By.xpath("//div[@id='bookFlightTab']//li[@class='adult-pax-list']//button[contains(@class, 'pax-plus')]")).click();  //click once to have 2 adults
+		driver.findElement(By.xpath("//div[@id='bookFlightTab']//li[@class='adult-pax-list']//button[contains(@class, 'pax-plus')]")).click();  //click twice to have 3 adults
+		
+		
+		//For child
+		////div[@id='bookFlightTab']//li[@class='child-pax-list']//button[contains(@class, 'pax-plus')]
+		//Default child count = 0 (cannot lower it).
+		driver.findElement(By.xpath("//div[@id='bookFlightTab']//li[@class='child-pax-list']//button[contains(@class, 'pax-plus')]")).click();  //click once to have 1 child
+		driver.findElement(By.xpath("//div[@id='bookFlightTab']//li[@class='child-pax-list']//button[contains(@class, 'pax-plus')]")).click();  //click twice to have 2 children
+		
+		//Aug 18, 2019 - some reason this stopped working.  There was an additional pop-up Help element, which cannot be dismiss
+		//Exception in thread "main" org.openqa.selenium.WebDriverException: unknown error: Element is not clickable at point (647, 688)
+		//  (Session info: chrome=75.0.3770.80)
+		//Click 'Done' button to close the passenger dropdown
+		//xpath = //div[@id='bookFlightTab']//button[text()='Done']
+		//driver.findElement(By.xpath("//div[@id='bookFlightTab']//button[text()='Done']")).click();
+		
+		//Work-around - to suppress passenger selection dropdown.  Click somewhere outside the dropdown
+		driver.findElement(By.xpath("//div[@id='bookFlightTab']//h4")).click();
+		
+		
+		//Click Search Flight button
+		//xpath = //div[@id='bookFlightTab']//span[text()='Search Flight']
+		driver.findElement(By.xpath("//div[@id='bookFlightTab']//span[text()='Search Flight']")).click();
+		
+		
+		//TODO From here - 8/13 5pm
+		
+		//Wait for the search result page to load
+		FluentWait<WebDriver> fWait = new FluentWait<WebDriver>(driver);
+		fWait.withTimeout(120, TimeUnit.SECONDS)
+				.pollingEvery(5, TimeUnit.SECONDS);
+		
+		
+		//Thread.sleep(60000);
+		//driver.quit();
 		
 	}
 
